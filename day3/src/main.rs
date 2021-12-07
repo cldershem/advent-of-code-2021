21939bbc891c331074c7fba781e6c36cf3d329f1
Cameron Dershem <cameron@pinkhatbeard.com>
2021-12-06 21:41:29 -0500

    day3 - part 2 - not working.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,13 +1,18 @@
 fn vec_of_bits_to_int(bits: Vec<u8>) -> u64 {
-    let out = bits
-        .iter()
+    bits.iter()
         .fold((0, 1), |(acc, mul), &bit| {
             (acc + (mul * (1 & bit as u64)), mul.wrapping_add(mul))
         })
-        .0;
+        .0
+}
 
-    dbg!(&out);
-    out
+// stolen from https://old.reddit.com/r/rust/comments/3xgeo0/biginner_question_how_can_i_get_the_value_of_a/
+fn get_bit_at(input: u64, n: u8) -> u8 {
+    if n < 64 && (input & (1 << n) != 0) {
+        1
+    } else {
+        0
+    }
 }
 
 fn power_consumption(input: &str) -> u64 {
@@ -54,92 +59,84 @@ fn power_consumption(input: &str) -> u64 {
         })
         .collect();
 
-    let gamma_rate_decimal = vec_of_bits_to_int(gamma_rate);
-    let epsilon_rate_decimal = vec_of_bits_to_int(epsilon_rate);
-
-    gamma_rate_decimal * epsilon_rate_decimal
+    vec_of_bits_to_int(gamma_rate) * vec_of_bits_to_int(epsilon_rate)
 }
 
-// stolen from https://old.reddit.com/r/rust/comments/3xgeo0/biginner_question_how_can_i_get_the_value_of_a/
-fn get_bit_at(input: u64, n: u8) -> u8 {
-    if n < 64 && (input & (1 << n) != 0) {
-        1
-    } else {
-        0
+fn most_common_bit_values(current_list: &[u64]) -> Vec<u64> {
+    let columns: Vec<Vec<_>> = (0..12)
+        .map(|bit_index| {
+            current_list
+                .iter()
+                .map(move |number| get_bit_at(*number, bit_index))
+                .collect()
+        })
+        .collect();
+
+    columns
+        .iter()
+        .map(|col| {
+            let zero_count = col.iter().filter(|x| **x == 0).count();
+            let one_count = col.iter().filter(|x| **x == 1).count();
+
+            if zero_count < one_count {
+                0
+            } else {
+                1
+            }
+        })
+        .collect()
+}
+
+fn narrow_down_list(current_list: &mut Vec<u64>, step: u8) -> Vec<u64> {
+    let most_common_bit_values = most_common_bit_values(current_list);
+
+    let mut new_list: Vec<_> = vec![];
+
+    // current_list
+    //     .iter()
+    //     .cloned()
+    //     .filter(|number| get_bit_at(*number, step) == (most_common_bit_values[step as usize]) as u8)
+    //     .collect()
+
+    for number in current_list {
+        if get_bit_at(*number, step) == (most_common_bit_values[step as usize]) as u8 {
+            new_list.push(*number);
+        }
     }
+
+    dbg!(new_list.len());
+    new_list
 }
 
-// fn narrow_down_most(current_list: Vec<u64>, step: u8) -> Vec<u64> {
-//     if current_list.len() == 1 {
-//         current_list
-//     } else {
-//         for item in current_list {
-//             let col = current_list.iter().map(|line| line[step]).collect();
-//             let zero_count = col.iter().filter(|x| **x == '0').count();
-//             let one_count = col.iter().filter(|x| **x == '1').count();
+fn oxygen_gen_rating(input: &str) -> u64 {
+    let mut current_list: Vec<_> = input
+        .lines()
+        .map(|num| isize::from_str_radix(num, 2).unwrap() as u64)
+        .collect();
 
-//         // if zero_count > one_count {
-//         //     0
-//         // } else {
-//         //     1
-//         // }
-//         }
-//     }
-// }
+    for bit_index in 0..12 {
+        if current_list.len() == 1 {
+            break;
+        }
 
-// fn oxygen_gen_rating(input: &str) -> u64 {
-//     let mut columns = vec![Vec::new(); 12];
+        let new_list = narrow_down_list(&mut current_list, bit_index);
+        dbg!(new_list.len());
+        current_list = new_list;
+    }
+    dbg!(&current_list);
 
-//     let mut current_list: Vec<_> = input
-//         .lines()
-//         .map(|num| num.parse::<u64>().unwrap())
-//         .collect();
-
-//     for (number_index, number) in current_list.iter().enumerate() {
-//         for bit_index in 0..12 {
-//             let bit_value = get_bit_at(*number, bit_index);
-//             columns[bit_index as usize].push(bit_value);
-//         }
-//     }
-
-//     dbg!(columns.len());
-//     dbg!(columns[0].len());
-
-//     // let gamma_rate: [u8; 12];
-
-//     // dbg!(current_list);
-
-//     // while current_list.len() > 1 {
-//     //     for place in 1..=12 {
-//     //         current_list = narrow_down_most(current_list, place);
-//     //     }
-//     // }
-
-//     // let most_common_nums: Vec<_> = columns
-//     //     .iter()
-//     //     .enumerate()
-//     //     .map(|(index, col)| {
-//     //         let zero_count = col.iter().filter(|x| **x == '0').count();
-//     //         let one_count = col.iter().filter(|x| **x == '1').count();
-
-//     //         if zero_count > one_count {
-//     //             0
-//     //         } else {
-//     //             1
-//     //         }
-//     //     })
-//     //     .collect();
-
-//     // dbg!(most_common_nums);
-
-//     0
-// }
+    current_list[0]
+}
 
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
 
-    let power_consumption = power_consumption(&input);
-    dbg!(power_consumption);
+    // let power_consumption = power_consumption(&input);
+    // dbg!(power_consumption);
 
-    // oxygen_gen_rating(&input);
+    let ox_rating = oxygen_gen_rating(&input);
+    dbg!(ox_rating);
+
+    // let co_rating = co2_scrubber_rating(&input);
+    // dbg!(co_rating);
 }
